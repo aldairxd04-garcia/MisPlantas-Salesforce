4950754a05de16d49360a8aa3fc43afca4f706ad
Aldair García <aldairgl06@gmail.com>
2025-12-22 20:01:07 -0600

    add param query to plantas app

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -2,8 +2,21 @@ import { LightningElement, wire } from 'lwc';
 import getAllSpecies from '@salesforce/apex/SpeciesService.getAllSpecies';
 
 export default class SpeciesList extends LightningElement {
+    //PROPS, GETS AND SETS
+    searchText;
+
+    //LIFECYCLE HOOKS
+
+    //WIRE
     //decorador para almacenar la consukta de getAllSpecies
-    @wire(getAllSpecies) 
+    @wire(getAllSpecies, {queryText: '$searchText'}) 
     species;
 
+    //METHODS
+    handleSearchInputValue(evt){
+        const keySearch = evt.detail.value;
+        if(keySearch.length > 2 || keySearch === ''){
+            this.searchText = keySearch;
+        }
+    }
 }
--- force-app/main/default/lwc/speciesTitle/speciesTitle.js
+++ force-app/main/default/lwc/speciesTitle/speciesTitle.js
@@ -1,9 +1,38 @@
 import { api, LightningElement } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class SpeciesTitle extends LightningElement {
+export default class SpeciesTitle extends NavigationMixin(LightningElement) {
     @api specie;
 
     connectedCallback() {
         console.log('Specie recibida:', JSON.stringify(this.specie));
     }
+
+    get id(){
+        return this.specie.Id;
+    }
+
+    get isOutdoors(){
+        return this.specie.Location__c.includes('Outdoors');
+    }
+
+    get isIndoors(){
+        return this.specie.Location__c.includes('Indoors');
+    }
+
+    navigateToRecordViewPage() {
+    // View a custom object record.
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: this.id,
+        objectApiName: "Species__c", // objectApiName is optional
+        actionName: "view",
+      },
+    });
+  }
+
+  handleNavigateSpecieDetail(){
+    this.navigateToRecordViewPage();
+  }
 }
